0c46869ddd88612d34559566f7735fa34fb38672
Ajay Kumar Maddukuri <40816420+MAJAYKUMAR@users.noreply.github.com>
2024-06-17 15:18:36 -0500

    Commit #1
    Added updates and chat bot.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# AjayPortfolio
+## Portfolio-Website
+Portfolio website built using HTML5, CSS3, JavaScript, and jQuery.
+
+
+
+
+<h2>📬 Contact</h2>
+
+Feel free to reach me through the below handles if you'd like to contact me.
---
+++ assets/js/script.js
@@ -0,0 +1,367 @@
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+
+        // scroll spy
+        $('section').each(function () {
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if (top > offset && top < offset + height) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500, 'linear')
+    });
+
+    
+
+    // <!-- emailjs to mail contact form data -->
+    $("#contact-form").submit(function (event) {
+
+        emailjs.sendForm('service_svbeezs', 'template_d96726w', '#contact-form','vqUsivpClqqhVsE-t')
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                document.getElementById("contact-form").reset();
+                alert("Form Submitted Successfully");
+            }, function (error) {
+                console.log('FAILED...', error);
+                alert("Form Submission Failed! Try Again");
+            });
+        event.preventDefault();
+    });
+    // <!-- emailjs to mail contact form data -->
+
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Portfolio | Ajay Kumar";
+            $("#favicon").attr("href", "assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "assets/images/favhand.png");
+        }
+    });
+
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["Full stack development","frontend development", "backend development", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
+
+async function fetchData(type = "skills") {
+    let response
+    type === "skills" ?
+        response = await fetch("skills.json")
+        :
+        response = await fetch("./projects/projects.json")
+    const data = await response.json();
+    return data;
+}
+
+function showSkills(skills) {
+    let skillsContainer = document.getElementById("skillsContainer");
+    let skillHTML = "";
+    skills.forEach(skill => {
+        skillHTML += `
+        <div class="bar">
+              <div class="info">
+                <img src=${skill.icon} alt="skill" />
+                <span>${skill.name}</span>
+              </div>
+            </div>`
+    });
+    skillsContainer.innerHTML = skillHTML;
+}
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector("#work .box-container");
+    let projectHTML = "";
+    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
+        projectHTML += `
+        <div class="box tilt">
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>`
+    });
+    projectsContainer.innerHTML = projectHTML;
+
+    // <!-- tilt js effect starts -->
+    VanillaTilt.init(document.querySelectorAll(".tilt"), {
+        max: 15,
+    });
+    // <!-- tilt js effect ends -->
+
+    /* ===== SCROLL REVEAL ANIMATION ===== */
+    const srtop = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 1000,
+        reset: true
+    });
+
+    /* SCROLL PROJECTS */
+    srtop.reveal('.work .box', { interval: 200 });
+
+}
+
+fetchData().then(data => {
+    showSkills(data);
+});
+
+fetchData("projects").then(data => {
+    showProjects(data);
+});
+
+// <!-- tilt js effect starts -->
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+});
+// <!-- tilt js effect ends -->
+
+
+// pre loader start
+// function loader() {
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
+// function fadeOut() {
+//     setInterval(loader, 500);
+// }
+// window.onload = fadeOut;
+// pre loader end
+
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+// Start of Tawk.to Live Chat
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/666f4be5981b6c56477df68b/1i0hc0tpu';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+// End of Tawk.to Live Chat
+
+
+/* ===== SCROLL REVEAL ANIMATION ===== */
+const srtop = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+
+/* SCROLL HOME */
+srtop.reveal('.home .content h3', { delay: 200 });
+srtop.reveal('.home .content p', { delay: 200 });
+srtop.reveal('.home .content .btn', { delay: 200 });
+
+srtop.reveal('.home .image', { delay: 400 });
+srtop.reveal('.home .linkedin', { interval: 600 });
+srtop.reveal('.home .github', { interval: 800 });
+srtop.reveal('.home .twitter', { interval: 1000 });
+srtop.reveal('.home .telegram', { interval: 600 });
+srtop.reveal('.home .instagram', { interval: 600 });
+srtop.reveal('.home .dev', { interval: 600 });
+
+/* SCROLL ABOUT */
+srtop.reveal('.about .content h3', { delay: 200 });
+srtop.reveal('.about .content .tag', { delay: 200 });
+srtop.reveal('.about .content p', { delay: 200 });
+srtop.reveal('.about .content .box-container', { delay: 200 });
+srtop.reveal('.about .content .resumebtn', { delay: 200 });
+
+
+/* SCROLL SKILLS */
+srtop.reveal('.skills .container', { interval: 200 });
+srtop.reveal('.skills .container .bar', { delay: 400 });
+
+/* SCROLL EDUCATION */
+srtop.reveal('.education .box', { interval: 200 });
+
+/* SCROLL PROJECTS */
+srtop.reveal('.work .box', { interval: 200 });
+
+/* SCROLL EXPERIENCE */
+srtop.reveal('.experience .timeline', { delay: 400 });
+srtop.reveal('.experience .timeline .container', { interval: 400 });
+
+/* SCROLL CONTACT */
+srtop.reveal('.contact .container', { delay: 400 });
+srtop.reveal('.contact .container .form-group', { delay: 400 });
+
+
+$(document).ready(function() {
+
+    //sticky header
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1) {
+          $(".header-area").addClass("sticky");
+        } else {
+          $(".header-area").removeClass("sticky");
+        }
+    
+        // Update the active section in the header
+        updateActiveSection();
+      });
+    
+      $(".header ul li a").click(function(e) {
+        e.preventDefault(); 
+    
+        var target = $(this).attr("href");
+    
+        if ($(target).hasClass("active-section")) {
+          return; 
+        }
+    
+        if (target === "#home") {
+          $("html, body").animate(
+            {
+              scrollTop: 0 
+            },
+            500
+          );
+        } else {
+          var offset = $(target).offset().top - 40; 
+    
+          $("html, body").animate(
+            {
+              scrollTop: offset
+            },
+            500
+          );
+        }
+    
+        $(".header ul li a").removeClass("active");
+        $(this).addClass("active");
+      });
+    
+  
+      //Initial content revealing js
+      ScrollReveal({
+        distance: "100px",
+        duration: 2000,
+        delay: 200
+      });
+    
+      ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
+        origin: "left"
+      });
+      ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
+        origin: "right"
+      });
+      ScrollReveal().reveal(".project-title, .contact-title", {
+        origin: "top"
+      });
+      ScrollReveal().reveal(".projects, .contact", {
+        origin: "bottom"
+      });
+  
+    //contact form to excel sheet
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzXP3lu3WMPIMuLtgj-7IP_510fap8C7lRoDrVDaHnhooULZO_Rr895JyZNdC8pI-gg/exec';
+    const form = document.forms['submitToGoogleSheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function () {
+                    msg.innerHTML = ""
+                }, 5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+    })
+      
+    });
+    
+    function updateActiveSection() {
+      var scrollPosition = $(window).scrollTop();
+    
+      // Checking if scroll position is at the top of the page
+      if (scrollPosition === 0) {
+        $(".header ul li a").removeClass("active");
+        $(".header ul li a[href='#home']").addClass("active");
+        return;
+      }
+    
+      // Iterate through each section and update the active class in the header
+      $("section").each(function() {
+        var target = $(this).attr("id");
+        var offset = $(this).offset().top;
+        var height = $(this).outerHeight();
+    
+        if (
+          scrollPosition >= offset - 40 &&
+          scrollPosition < offset + height - 40
+        ) {
+          $(".header ul li a").removeClass("active");
+          $(".header ul li a[href='#" + target + "']").addClass("active");
+        }
+      });
+    }
+    
+  
+   
